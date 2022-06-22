2ade77e9d488aae1695e6adaa41ddeb6f7b569be
Flippie.Coetser <flippie.coetser@gmail.com>
2022-06-22 05:04:57 -0700

    update demo

--- demo/index.js
+++ demo/index.js
@@ -1,3 +1,2 @@
-import { Calculator } from 'calculator';
-console.log(`Import module: Calculator`)
-console.log(`Use module: 1 + 2 = ${Calculator.Add(1, 2)}`); 
+import { Event } from 'events';
+let event = new Event()
